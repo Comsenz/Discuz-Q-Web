247494f9926691d106c5f8d3d85501dc8c848835
chenpeiguang <434770438@qq.com>
2020-08-05 15:41:07 +0800

    refactor: store & router

--- src/router/index.js
+++ src/router/index.js
@@ -30,7 +30,17 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return {
+        x: 0,
+        y: 0
+      }
+    }
+  }
 })
 
 export default router
