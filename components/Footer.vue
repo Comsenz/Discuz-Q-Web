0264976e65ae69b4cfa72a0bd77dcec1dc46e87a
BruceLuo <295448382@qq.com>
2020-08-06 17:02:35 +0800

    feat: components - footer style

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>footer</div>
+  <div class="footer">
+    <span> Powered By &nbsp  <span class="bold">Discuz!Q</span> &nbsp    © 2014-2019 &nbsp 分享&nbsp  / 交流 &nbsp /&nbsp  共融 </span>
+  </div>
 </template>
+
+<style lang="scss">
+.footer{
+  padding: 30px 0;
+  text-align: center;
+  color: #8590A6;
+
+  span .bold{
+    font-weight: bold;
+  }
+
+}
+</style>
