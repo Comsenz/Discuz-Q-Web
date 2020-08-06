ca722a31853153e3289f2624319dd991208b196b
chenpeiguang <434770438@qq.com>
2020-08-06 18:10:48 +0800

    feat: Store - demo

--- pages/demo.vue
+++ pages/demo.vue
@@ -5,12 +5,16 @@
     <el-button type="success">成功按钮</el-button>
     <el-button type="info">信息按钮</el-button>
     <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
+    <el-button type="danger" @click="storeCommit">store commit</el-button>
     <p>svg</p>
     <div>
       <svg-icon style="font-size: 100px;" type="alipay" />
     </div>
     <div>{{info}}</div>
+
+    <p>store commit</p>
+    <el-button type="primary" @click="storeCommit">store commit</el-button>
+    <div>{{$store.state.demo.counter}}</div>
   </div>
 </template>
 
@@ -34,6 +38,9 @@ export default {
         console.log('res =>', res.data.data)
         this.info = JSON.stringify(res.data.data || {})
       })
+    },
+    storeCommit() {
+      this.$store.commit('demo/increment')
     }
   }
 }
