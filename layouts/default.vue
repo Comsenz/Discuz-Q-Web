f14532ad191aaf91bd3ed2be9be93820467dfad0
chenpeiguang <434770438@qq.com>
2020-08-09 19:20:42 +0800

    feat: Views - layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,23 @@
 <template>
   <div>
     <Header/>
-    <div class="container">
+    <div class="app-cont">
       <Nuxt/>
     </div>
     <Footer/>
   </div>
 </template>
 
-<style>
-  .container {
-    display: flex;
-    justify-content: center;
-    margin: 15px auto 0;
-    width: 1005px;
-    /* height: 706px; */
-    min-height: 660px;
-    background: rgba(255, 255, 255, 1);
-    box-shadow: 0 3px 3px rgba(0, 0, 0, 0.03);
-    opacity: 1;
-    border-radius: 5px;
-  }
+<style lang="scss" scoped>
+.app-cont {
+  display: flex;
+  justify-content: center;
+  margin: 15px auto 0;
+  width: 1005px;
+  /* height: 706px; */
+  min-height: calc(100vh - 65px - 80px - 15px);
+  background: rgba(255, 255, 255, 1);
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.03);
+  border-radius: 5px;
+}
 </style>
