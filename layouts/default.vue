3a91c421dae97379dbc252465146bfd9443235f1
BruceLuo <295448382@qq.com>
2020-08-10 10:03:46 +0800

    style: default - style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
-<<<<<<< HEAD
     <Header :head-img="info.set_site ? info.set_site.site_header_logo : ''" :key="this.$route.path"/>
-    <div class="container">
-=======
-    <Header/>
     <div class="app-cont">
->>>>>>> f14532ad191aaf91bd3ed2be9be93820467dfad0
       <Nuxt/>
     </div>
     <Footer/>
   </div>
 </template>
-<<<<<<< HEAD
+
 <script>
 export default {
   data() {
@@ -33,21 +28,6 @@ export default {
   }
 }
 </script>
-<style>
-  .container {
-    display: flex;
-    justify-content: center;
-    margin: 15px auto 0;
-    width: 1005px;
-    /* height: 706px; */
-    min-height: 660px;
-    background: rgba(255, 255, 255, 1);
-    box-shadow: 0 3px 3px rgba(0, 0, 0, 0.03);
-    opacity: 1;
-    border-radius: 5px;
-  }
-=======
-
 <style lang="scss" scoped>
 .app-cont {
   display: flex;
@@ -60,5 +40,4 @@ export default {
   box-shadow: 0 3px 3px rgba(0, 0, 0, 0.03);
   border-radius: 5px;
 }
->>>>>>> f14532ad191aaf91bd3ed2be9be93820467dfad0
 </style>
