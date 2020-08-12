cfb13e4c4aa8417a83ee1d644ec5823257c8b92c
linrenyan <linrenyan@yeezon.com>
2020-08-12 14:49:18 +0800

    feat:  add custom_layouts

---
+++ layouts/custom_layout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <Header
+      :key="this.$route.path"
+      :head-img="info.set_site ? info.set_site.site_header_logo : ''"
+    />
+    <div class="app-cont">
+      <Nuxt />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      info: ''
+    }
+  },
+  mounted() {
+    const _params = {
+      _jv: {
+        type: 'forum'
+      }
+    }
+    this.$store.dispatch('jv/get', _params).then(data => {
+      console.log('user data => ', data)
+      this.info = data
+    })
+  }
+}
+</script>
+<style lang="scss" scoped>
+.app-cont {
+  display: flex;
+  justify-content: center;
+  margin: 15px auto 0;
+  width: 1005px;
+  /* height: 706px; */
+  min-height: calc(100vh - 65px - 80px - 15px);
+}
+</style>
