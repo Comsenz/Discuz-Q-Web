9312b6661d6fad3c4b17074610c321a3d9f10782
linrenyan <linrenyan@yeezon.com>
2020-09-16 11:13:04 +0800

    fix: copyright

--- components/Copyright.vue
+++ components/Copyright.vue
@@ -66,7 +66,7 @@ export default {
       border-top: 1px solid $border-color-base;
       margin-top: 17px;
       padding-top: 15px;
-      line-height: 15px;
+      line-height: 20px;
       > span {
         &:last-child {
           margin-left: 20px;
