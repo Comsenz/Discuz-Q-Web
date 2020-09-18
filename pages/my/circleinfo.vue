9f3be60d93658881cd8b87d0fefa87055eadda7a
linrenyan <linrenyan@yeezon.com>
2020-09-18 11:41:01 +0800

    fix: circleinfo

--- pages/my/circleinfo.vue
+++ pages/my/circleinfo.vue
@@ -56,7 +56,7 @@
             <div class="role">{{ $t('site.role') }}:{{ forums.user && forums.user.groups && forums.user.groups.length > 0 && forums.user.groups[0].name || '' }}</div>
             <div v-if="userInfo.joinedAt" class="join-time">{{ $t('manage.joinedTime') }}:{{ userInfo.joinedAt.substr(0, 10) }}
               <template v-if="forums && forums.set_site.site_mode === 'pay'">,{{ $t('site.periodvalidity') + $t('site.to') + handleExpiredAt(userInfo.expiredAt) }}
-                <template v-if="userInfo.expiredAt">({{ $t('pay.surplus') + (handleDays('userInfo.expiredAt') > 0 ? handleDays('userInfo.expiredAt') : 0) + $t('site.day') }})</template>
+                <!-- <template v-if="userInfo.expiredAt">({{ $t('pay.surplus') + (handleDays('userInfo.expiredAt') > 0 ? handleDays('userInfo.expiredAt') : 0) + $t('site.day') }})</template> -->
               </template>
             </div>
             <div class="permission-list">
