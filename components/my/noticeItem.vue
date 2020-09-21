83b1272bda4c35730351c5aea2fb96010bf90f0d
linrenyan <linrenyan@yeezon.com>
2020-09-21 14:40:44 +0800

    fix: 主题详情打开新页面

--- components/my/noticeItem.vue
+++ components/my/noticeItem.vue
@@ -26,7 +26,7 @@
         <div class="time">{{ timerDiff(item.created_at) + $t('topic.before') }}</div>
       </div>
       <nuxt-link v-if="item.post_content" :to="`/topic/comment?threadId=${item.thread_id}&commentId=${item.reply_post_id !== 0 ? item.reply_post_id : item.post_id}`" class="post-content" v-html="item.post_content" />
-      <nuxt-link v-if="(item.thread_title || item.content) && item.type !== 'system'" :to="`/topic/${item.thread_id}`" class="thread">
+      <nuxt-link v-if="(item.thread_title || item.content) && item.type !== 'system'" :to="`/topic/${item.thread_id}`" class="thread" target="_blank">
         <div class="thread-user-name">{{ item.thread_username }}:</div>
         <div class="thread-title">
           <div v-if="item.thread_title" v-html="item.thread_title" />
