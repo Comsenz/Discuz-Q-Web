4a8de437b0dbd724694c53c2646fdf76a6911606
kcvo <943514589@qq.com>
2020-09-21 22:56:03 +0800

    bug: fix after thread srr error, ajax

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
   // https://discuz.chat https://dq.comsenz-service.com
   env: {
     mobileDomain: process.env.NODE_ENV !== 'production' ? 'https://dq.comsenz-service.com' : 'https://discuz.chat',
-    baseURL: process.env.NODE_ENV !== 'production' ? 'https://dq.comsenz-service.com/api' : '/api'
+    baseURL: process.env.NODE_ENV !== 'production' ? 'https://discuz.chat/api' : '/api'
   },
   /*
   ** Nuxt rendering mode
