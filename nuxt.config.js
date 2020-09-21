c803339a7ffe08bb2c9fcc28aae3ee5f8ceb6248
linrenyan <linrenyan@yeezon.com>
2020-09-21 20:15:05 +0800

    fix: nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ function resolve(dir) {
 export default {
   // https://discuz.chat https://dq.comsenz-service.com
   env: {
-    mobileDomain: 'https://dq.comsenz-service.com',
+    mobileDomain: process.env.NODE_ENV !== 'production' ? 'https://dq.comsenz-service.com' : 'https://discuz.chat',
     baseURL: process.env.NODE_ENV !== 'production' ? 'https://dq.comsenz-service.com/api' : '/api'
   },
   /*
