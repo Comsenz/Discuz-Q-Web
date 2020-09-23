1f1f09686545a55c00e71a764cb4c4c8951451df
chenpeiguang <434770438@qq.com>
2020-09-23 11:28:44 +0800

    feat: some

--- middleware/header.js
+++ middleware/header.js
@@ -1,11 +1,17 @@
-// header.js
+
 import axios from 'axios'
 import cookie from '../utils/parserCookie'
+import env from '@/utils/env.js'
+
 export default function(context) {
   if (context && context.req && context.req.headers) {
     const token = cookie.get('token', context.req.headers.cookie)
+
     if (token) {
       axios.defaults.headers['authorization'] = `Bearer ${token}`
     }
+
+    // Set Host
+    env.host = context.req.headers.host || ''
   }
 }
