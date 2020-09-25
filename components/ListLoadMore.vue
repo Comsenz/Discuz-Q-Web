1d6646fc65ebe4f909ed19b0997db47790634124
linrenyan <linrenyan@yeezon.com>
2020-09-25 15:06:07 +0800

    fix: add 加载更多组件

---
+++ components/ListLoadMore.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="load-more-container">
+    <loading v-if="loading" />
+    <template v-else>
+      <div v-if="hasMore && pageNum % 5 === 0" class="load-more" @click="loadMore">{{ $t('topic.showMore') }}</div>
+      <loading v-else-if="hasMore && pageNum % 5 > 0" />
+      <div v-else class="no-more"><svg-icon v-if="length === 0" type="empty" class="empty-icon" />{{ length > 0 ? $t('discuzq.list.noMoreData') : $t('discuzq.list.noData') }}</div>
+    </template>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    hasMore: {
+      type: Boolean,
+      default: false
+    },
+    pageNum: {
+      type: [Number, String],
+      default: 1
+    },
+    length: {
+      type: [Number, String],
+      default: 0
+    }
+  },
+  methods: {
+    loadMore() {
+      this.$emit('loadMore')
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -18,11 +18,7 @@
       </div>
       <div class="post-list">
         <post-item v-for="(item, index) in threadsData" :key="index" :item="item" />
-        <loading v-if="loading" />
-        <template v-else>
-          <div v-if="hasMore && pageNum % 5 === 0" class="load-more" @click="loadMore">{{ $t('topic.showMore') }}</div>
-          <div v-else class="no-more"><svg-icon v-if="threadsData.length === 0" type="empty" class="empty-icon" />{{ threadsData.length > 0 ? $t('discuzq.list.noMoreData') : $t('discuzq.list.noData') }}</div>
-        </template>
+        <list-load-more :loading="loading" :has-more="hasMore" :page-num="pageNum" :length="threadsData.length" @loadMore="loadMore" />
       </div>
     </main>
     <aside class="cont-right">
