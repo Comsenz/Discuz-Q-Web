165eab1c33e495ba7be70e7ff670a8e7c7e8d9ed
BruceLuo <295448382@qq.com>
2020-10-14 16:43:50 +0800

    refactor: pages changename

---
+++ pages/pages/modify/resetpwdsuccess.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="page-findpwd">
+    <div class="title"> {{ $t('modify.resetPasswordSuccess') }}</div>
+    <div class="describe"> {{ $t('modify.user') + ' “' + username + '” ' + $t('modify.resetPasswordSuccessRepeat') }} !</div>
+    <div class="detail"> {{ $t('modify.resetPasswordLoginTip') }}</div>
+    <nuxt-link
+      to="/pages/user/login"
+      class="login"
+    >
+      <el-button
+        type="primary"
+        class="btn"
+      >
+        {{ $t('user.login') }}
+      </el-button>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResetPwdSuccess',
+  computed: {
+    username() {
+      if (process.client) return window.localStorage.getItem('username') || ''
+      return ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page-findpwd {
+  margin: 60px auto 0;
+  height: 300px;
+  width: 300px;
+  .btn {
+    color: #1878f3;
+    background: #ffffff !important;
+    border: 1px solid #1878f3 !important;
+    border-color: #1878f3 !important;
+    border-radius: 2px;
+  }
+  // .btn :hover{
+  // }
+  > .title {
+    font-weight: bold;
+    color: #343434;
+    font-size: 26px;
+  }
+
+  > .describe {
+    color: #000;
+    margin-top: 50px;
+    font-size: 18px;
+  }
+
+  > .detail {
+    margin-top: 20px;
+    color: #909399;
+  }
+
+  > .login {
+    display: block;
+    margin-top: 20px;
+  }
+}
+</style>
