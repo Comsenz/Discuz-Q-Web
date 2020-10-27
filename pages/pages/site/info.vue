c9f5d073d79997cc1095b9dbb1a3f4428969600c
BruceLuo <295448382@qq.com>
2020-10-27 10:59:52 +0800

    fix: pages - fix withdrawal

--- pages/pages/site/info.vue
+++ pages/pages/site/info.vue
@@ -26,6 +26,7 @@
           <span class="date color">{{ $t('site.circlemaster') }}</span>
           <span class="img">
             <Avatar
+              v-if="forums.set_site && forums.set_site.site_author"
               :user="{
                 username: forums.set_site && forums.set_site.site_author && forums.set_site.site_author.username || '',
                 avatarUrl: forums.set_site && forums.set_site.site_author && forums.set_site.site_author.avatar || '',
@@ -34,6 +35,7 @@
               :round="true"
               class="avatar"
             />
+            <avatar v-else :user="{ id: 0, username: '无', avatarUrl: ''}" :prevent-jump="true" :size="30" :round="true" />
           </span>
           <span class="workdate3">
             {{ forums.set_site && forums.set_site.site_author && forums.set_site.site_author.username || '' }}
