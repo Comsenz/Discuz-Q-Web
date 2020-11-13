c8a3b8cc88561183835a9d72df57eaef9480dbba
linrenyan <linrenyan@yeezon.com>
2020-11-13 11:55:43 +0800

    fix: routerHash

--- plugins/routerHash.js
+++ plugins/routerHash.js
@@ -11,5 +11,50 @@ export default [
   { // 评论详情页
     h5: '/pages/topic/comment',
     pc: '/thread/comment'
+  },
+  { // 搜索结果页
+    h5: '/pages/site/search',
+    pc: '/site/search'
+  },
+  { // 搜索用户列表页
+    h5: '/pages/site/search-user',
+    pc: '/site/search-user'
+  },
+  { // 位置信息页
+    h5: '/pages/topic/position',
+    pc: '/topic/position'
+  },
+  { // 话题详情页
+    h5: '/pages/topic/content',
+    pc: '/topic',
+    dynamic: 'id'
+  },
+  { // 话题列表页
+    h5: '/pages/topic/list',
+    pc: '/topic/list'
+  },
+  { // 站点信息页
+    h5: '/pages/site/index',
+    pc: '/site'
+  },
+  { // 站点管理页
+    h5: '/pages/manage/index',
+    pc: '/manage'
+  },
+  { // 推广邀请页
+    h5: '/pages/invite/index',
+    pc: '/invite'
+  },
+  { // 我的收藏
+    h5: '/pages/my/favorite',
+    pc: '/my/favorite'
+  },
+  { // 消息页
+    h5: '/',
+    pc: '/my/notice'
+  },
+  { // 分类页
+    h5: '/',
+    pc: '/category'
   }
 ]
