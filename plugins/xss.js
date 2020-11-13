cc7c531f971b2b2b1c8bac6880f0c708b5e0ca8a
kcvo <943514589@qq.com>
2020-11-13 18:17:52 +0800

    fix: TODO list display

--- plugins/xss.js
+++ plugins/xss.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 // 自定义白名单
 xss.whiteList.img = ['src', 'alt', 'title', 'width', 'height', 'class']
 xss.whiteList.button = ['class']
-xss.whiteList.input = ['data-task-id', 'type']
+xss.whiteList.input = ['data-task-id', 'type', 'disabled']
 xss.whiteList.ul = ['class']
 xss.whiteList.li = ['class', 'data-task-state']
 
