4607a1fa9b0eae98dbd75924c9543629eb265f29
kcvo <943514589@qq.com>
2020-11-13 11:41:22 +0800

    fix: h5 route line

---
+++ plugins/routerHash.js
@@ -0,0 +1,15 @@
+export default [
+  { // 详情页
+    h5: '/topic/index',
+    pc: '/thread',
+    dynamic: 'id'
+  },
+  { // 发布页
+    h5: '/topic/post',
+    pc: '/thread/post'
+  },
+  { // 评论详情页
+    h5: '/pages/topic/comment',
+    pc: '/thread/comment'
+  }
+]
