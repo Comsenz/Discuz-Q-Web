cf237a72e69484f5421bf7ca3753d1668f9424e4
BruceLuo <295448382@qq.com>
2020-11-13 13:17:20 +0800

    fix: pages - fix logout

--- plugins/routerHash.js
+++ plugins/routerHash.js
@@ -1,15 +1,55 @@
-export default [
-  { // 详情页
-    h5: '/topic/index',
-    pc: '/thread',
-    dynamic: 'id'
-  },
-  { // 发布页
-    h5: '/topic/post',
-    pc: '/thread/post'
-  },
-  { // 评论详情页
-    h5: '/pages/topic/comment',
-    pc: '/thread/comment'
-  }
+export default [{ // 详情页
+  h5: '/topic/index',
+  pc: '/thread',
+  dynamic: 'id'
+},
+{ // 发布页
+  h5: '/topic/post',
+  pc: '/thread/post'
+},
+{ // 评论详情页
+  h5: '/pages/topic/comment',
+  pc: '/thread/comment'
+},
+{
+  h5: '/pages/user/register-bind',
+  pc: '/user/register-bind'
+},
+{
+  h5: '/pages/user/register-bind',
+  pc: '/user/register-bind'
+},
+{
+  h5: '/pages/user/register',
+  pc: '/user/register'
+},
+{
+  h5: '/pages/user/login',
+  pc: '/user/login'
+},
+{
+  h5: '/pages/profile/index',
+  pc: '/user',
+  dynamic: 'id'
+},
+{
+  h5: '/pages/my/wallet',
+  pc: '/my/wallet'
+},
+{
+  h5: '/pages/my/profile',
+  pc: '/my/profile'
+},
+{
+  h5: '/pages/my/shield',
+  pc: '/my/shield'
+},
+{
+  h5: '/pages/site/info',
+  pc: '/site/info'
+},
+{
+  h5: '/pages/site/partner-invite',
+  pc: '/site/partner-invite'
+}
 ]
